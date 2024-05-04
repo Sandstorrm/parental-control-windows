a980e0057663a25869ea44ddfa44995c77a1554f
Sandstorm <sandstorrm0@gmail.com>
2024-05-04 14:10:17 -0700

    updated update.py to run using pyw

--- update.py
+++ update.py
@@ -1,9 +1,8 @@
 import requests
 import subprocess
-import time
+import sys, os
 
 def update_file_from_url(url, filename):
-  subprocess.run(["taskkill", "/F", "/IM", "pythonw3.12.exe"])
 
   try:
     response = requests.get(url, stream=True)
@@ -16,6 +15,10 @@ def update_file_from_url(url, filename):
     print(f"File downloaded successfully: {filename}")
   except requests.exceptions.RequestException as e:
     print(f"Error downloading file: {e}")
+
+def run_pyw_script(script_path):
+    pythonw_executable = os.path.join(sys.prefix, "pythonw.exe")
+    subprocess.Popen([pythonw_executable, script_path])
   
 main_url = "https://raw.githubusercontent.com/Sandstorrm/parental-control/main/main.pyw"
 filename = "main.pyw"
@@ -23,7 +26,9 @@ filename = "main.pyw"
 settings_url = "https://raw.githubusercontent.com/Sandstorrm/parental-control/main/settings.json"
 settings_filename = "settings.json"
 
+subprocess.run(["taskkill", "/F", "/IM", "pythonw3.12.exe"])
+
 update_file_from_url(main_url, filename)
 update_file_from_url(settings_url, settings_filename)
 
-exec(open("main.pyw").read())
+run_pyw_script(os.path.abspath(filename))
