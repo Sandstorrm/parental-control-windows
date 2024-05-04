ce6f6082d77492cc1113e9d021342f0021677386
Sandstorm <sandstorrm0@gmail.com>
2024-05-04 12:48:43 -0700

    test

--- update.py
+++ update.py
@@ -16,7 +16,7 @@ def update_file_from_url(url, filename):
   except requests.exceptions.RequestException as e:
     print(f"Error downloading file: {e}")
 
-  subprocess.run(["pythonw3.12.exe main.pyw"])
+  exec(open("main.pyw").read())
   
 
 url = "https://raw.githubusercontent.com/Sandstorrm/parental-control/main/main.pyw"
